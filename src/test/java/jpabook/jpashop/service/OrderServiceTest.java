759f0637f7b3b260bec82d8250a9c42f2d6c1cae
박철현 <park82602@gmail.com>
2024-12-06 18:49:47 +0900

    feat : Order 비즈니스 로직 테스트 코드 생성

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() {
+        //given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
+        em.persist(member);
+
+        Item book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+        //when
+        int orderCounts = 2;
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCounts);
+
+        //then
+        Order getOrder = orderRepository.findById(orderId);
+        assertEquals(OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals(1, getOrder.getOrderItems().size());
+        assertEquals(book.getPrice() * orderCounts, getOrder.getTotalPrice());
+        assertEquals(8, book.getStockQuantity());
+    }
+}
